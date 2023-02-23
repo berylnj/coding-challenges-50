a5cd4323a13776a5a532e098e29ac63bd7fea91d
berylnj <ntengkehberyl@gmail.com>
2023-02-23 10:21:17 -0500

    Added ex3

---
+++ ex11.ts
@@ -0,0 +1,6 @@
+function displaypositive(numbers; number[])
+for(i=0;i<T.length;i++){
+    sum=sum+T[i]
+    average=sum/T.length;
+}
+console.log(average);
---
+++ ex12.ts
@@ -0,0 +1,6 @@
+function display positive(display: display[]){
+for(i=0;i!<0;i++)
+return ([i>0])
+}
+var output=positive(1,-2,3,-5,6,7,-8,9)
+console.log(output)
